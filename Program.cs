db924676f38860193e40cb2802a925867500444e
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2022-01-09 17:25:12 +0200

    darbas su programa

--- Program.cs
+++ Program.cs
@@ -139,34 +139,34 @@ namespace MetodasRegistracijosSistema
                         if (kategorija == 1)
                         {
                             string meetingoKategorija = "CodeMonkey";
-                            Console.WriteLine(meetingoKategorija);
+                            Console.WriteLine("Jūsų pasirinkta kategorija: {0}", meetingoKategorija);
                         }
                         if (kategorija == 2)
                         {
                             string meetingoKategorija = "Hub";
-                            Console.WriteLine(meetingoKategorija);
+                            Console.WriteLine("Jūsų pasirinkta kategorija: {0}", meetingoKategorija);
                         }
                         if (kategorija == 3)
                         {
                             string meetingoKategorija = "Short";
-                            Console.WriteLine(meetingoKategorija);
+                            Console.WriteLine("Jūsų pasirinkta kategorija: {0}", meetingoKategorija);
                         }
                         if (kategorija == 4)
                         {
                             string meetingoKategorija = "TeamBuilding";
-                            Console.WriteLine(meetingoKategorija);
+                            Console.WriteLine("Jūsų pasirinkta kategorija: {0}", meetingoKategorija);
                         }
                         Console.WriteLine("Pasirinkite susitikimo tipa (1 - Live, 2 - InPerson): ");
                         int meetingoTip = int.Parse(Console.ReadLine());
                         if (meetingoTip == 1)
                         {
                             string meetingoTipas = "Live";
-                            Console.WriteLine(meetingoTipas);
+                            Console.WriteLine("Jūsų pasirinktas susitikimo tipas: {0}", meetingoTipas);
                         }
                         if (meetingoTip == 2)
                         {
                             string meetingoTipas = "InPerson";
-                            Console.WriteLine(meetingoTipas);
+                            Console.WriteLine("Jūsų pasirinktas susitikimo tipas: {0}", meetingoTipas);
                         }
 
                         Console.WriteLine("Įveskite pradžios datą: ");
@@ -174,35 +174,79 @@ namespace MetodasRegistracijosSistema
                         Console.WriteLine("Įveskite pabaigos datą: ");
                         string meetingoPabaigosData = Console.ReadLine();
 
+                        Console.WriteLine("Jūsų susitikimas užregistruotas sėkmingai");
+
                     }
                     if (pasirinkimas == 2)
                     {
-                        Console.WriteLine("Istrinti meetinga is saraso");
+                        Console.Clear();
+                        Console.WriteLine("Istrinti meetinga is saraso \n");
                     }
                     if (pasirinkimas == 3)
                     {
-                        Console.WriteLine("meetingu sarasas rodomas cia");
-                    }
-                    if (pasirinkimas == 4)
-                    {
-                        Console.WriteLine("Prideti žmogų į susitikimą");
-                    }
-                    if (pasirinkimas == 5)
-                    {
-                        Console.WriteLine("Pašalinti žmogų iš susitikimo");
-                    }
-                    if (pasirinkimas == 6)
-                    {
-                        break;
+                        Console.Clear();
+                        Console.WriteLine("Susitikimų paieška \n");
+                        while (true)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(" 1 - Rodyti visus susitikimus \n 2 - Filtruoti pagal aprašymą \n 3 - Filtruoti pagal atsakingą asmenį \n 4 - Filtruoti pagal kategoriją \n 5 - Filtruoti pagal tipą \n 6 - Filtruoti pagal datas \n 7 - Filtruoti pagal žmonių kiekį \n 8 - Grižti atgal");
+                            Console.Write("Jusu pasirinkimas: ");
+                            int pasirink = int.Parse(Console.ReadLine());
+
+                            if (pasirink == 1)
+                            {
+                                Console.WriteLine(DB.meetingai);
+                            }
+                            if (pasirink == 2)
+                            {
+                                Console.WriteLine("Įveskite susitikimo aprašymą");
+                                Console.WriteLine(DB.meetingai.Where(x => x.Description == Console.ReadLine()));
+                            }
+                            if (pasirink == 3)
+                            {
+                                Console.WriteLine("Įveskite atsakingą asmenį");
+                            }
+                            if (pasirink == 4)
+                            {
+                                Console.WriteLine("Įveskite kategoriją");
+                            }
+                            if (pasirink == 5)
+                            {
+                                Console.WriteLine("Įveskite tipą");
+                            }
+                            if (pasirink == 6)
+                            {
+                                Console.WriteLine("Įveskite datas");
+                            }
+                            if (pasirink == 7)
+                            {
+                                Console.WriteLine("Įveskite žmonių kiekį");
+                            }
+                            if (pasirink == 8)
+                            {
+                                break;
+                            }
+                        }
+                        if (pasirinkimas == 4)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Prideti žmogų į susitikimą \n");
+                        }
+                        if (pasirinkimas == 5)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Pašalinti žmogų iš susitikimo \n");
+                        }
+                        if (pasirinkimas == 6)
+                        {
+                            break;
+                        }
                     }
                 }
             }
         }
-                
-            
         
-
-        
+                    
 
         public class Vartotojas
         {
